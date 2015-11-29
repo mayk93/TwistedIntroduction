b4e0a640836ce08a234ed16a981376821b593e03
Mihai Mandrescu <mihai.mandrescu@gmail.com>
2015-11-29 20:44:17 +0000

    Sync Text Client. Not working.

---
+++ SyncText/SimpleTextClient.py
@@ -0,0 +1,40 @@
+#!/usr/local/bin/python3
+
+import socket
+import datetime
+
+HOST = '104.155.75.83'
+PORT = 50007
+FILE = "text.txt"
+BATCH_SIZE = 1024
+
+def configure_socket():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((HOST,PORT))
+    return sock
+
+def get_text(sock):
+    text = ""
+    while True:
+        # While receiving data, the client is blocked.
+        data = sock.recv(BATCH_SIZE)
+        if not data:
+            sock.close()
+            break
+        print("Received at "+str(datetime.datetime.now.strftime("%Y-%m-%d %H:%M"))+": "+str(data))
+        text += data
+    return text
+
+def main():
+    start = datetime.datetime.now()
+    sock = configure_socket()
+    text = get_text(sock)
+    end = datetime.datetime.now()
+    print("Entire text received in: "+str(end-start))
+    print("Entire Text:")
+    print("\n--- * ---\n")
+    print(text)
+    print("\n--- * ---\n")
+
+if __name__ == '__main__':
+    main()
